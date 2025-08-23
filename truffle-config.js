c48e91d58ef8999a1b1c2237a26f99f38c9a548f
miss you <1020814597@qq.com>
2025-08-23 16:43:30 +0800

    Add Truffle configuration file

---
+++ truffle-config.js
@@ -0,0 +1,48 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+      gas: 6721975,
+      gasPrice: 20000000000,
+    },
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        ),
+      network_id: 11155111,
+      gas: 4000000,
+      gasPrice: 10000000000,
+    },
+    mainnet: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        ),
+      network_id: 1,
+      gas: 4000000,
+      gasPrice: 20000000000,
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.8.19',
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+  db: {
+    enabled: false,
+  },
+};
