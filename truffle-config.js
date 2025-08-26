46f3b50f6b91f6bb0cea903e1367de4094e0903e
juzhiqiang <1020814597@qq.com>
2025-08-26 11:27:45 +0800

    update： udpate

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 8545,
+      port: 7545,
       network_id: '*',
       gas: 6721975,
       gasPrice: 20000000000,
